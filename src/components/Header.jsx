69159c1819111fd8716f7d8818c15d086b209872
TechMrs <kemilat50@gmail.com>
2024-03-19 19:45:32 +0800

    chore: started with the header design

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import logo from '../assets/KCOAT.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+
+        <ul>
+          <li>Featured</li>
+          <div>
+            <li>Men</li>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+
+          <div>
+            <li>Women</li>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+          <div>
+            <li>Accessories</li>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1024px",
+      xl: "1366px",
+      "2xl": "1536px",
+    },
+
+    colors: {
+      "primary": "#fff",
+      "secondary": "#000",
+
+    }
   },
   plugins: [],
-}
+};
 
