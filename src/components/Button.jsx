2752bb91b3a5bebcf5d4195eec49d590155b4eb8
TechMrs <kemilat50@gmail.com>
2024-03-19 22:26:10 +0800

    Feat: create header and button components

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = () => {
+  return (
+    <div>
+      <button className="bg-tertiary rounded-md w-[2rem]"></button>
+    </div>
+  )
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#fff",
+      secondary: "#F2E29F",
+      tertiary: "#0C323A",
+      bland: "#000",
+      color: "#323232",
+    },
   },
   plugins: [],
-}
+};
 
