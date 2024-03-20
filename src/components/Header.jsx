21d365f08a2ff88a7071c9b2f4987d7f958b9fe5
Shaybeth2 <shaybeth2@gmail.com>
2024-03-20 05:06:07 +0100

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,29 +4,38 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
   return (
     <>
-      <div>
+      <div className="p-5 shadow md:flex  md:justify-between">
         <div>
           <img src={logo} alt="logo" />
         </div>
 
-        <ul>
-          <li>Featured</li>
-          <div>
-            <li>Men</li>
-            <FontAwesomeIcon icon={faChevronDown} />
+        <ul className="md:flex md:items-center md:z-auto md:static absolute w-full left-0 md:w-auto  md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+      
+          <li className="mx-4 my-6 md:my-0">Featured</li>
+        
+          <div className='flex'>
+            <li className='mx-4 my-6 md:my-0'>Men</li>
+            <FontAwesomeIcon icon={faChevronDown} className='ml-1'/>
           </div>
 
-          <div>
-            <li>Women</li>
-            <FontAwesomeIcon icon={faChevronDown} />
+          <div className='flex'>
+            <li className='mx-4 my-6 md:my-0'>Women</li>
+            <FontAwesomeIcon icon={faChevronDown} className='ml-1'/>
           </div>
-          <div>
-            <li>Accessories</li>
-            <FontAwesomeIcon icon={faChevronDown} />
+          <div className='flex'>
+            <li className='mx-4 my-6 md:my-0'>Accessories</li>
+            <FontAwesomeIcon icon={faChevronDown}  className='ml-1'/>
           </div>
-          <li>Contact</li>
+          <li className='mx-4 my-6 md:my-0'>Contact</li>
+          
         </ul>
+        <div className='space-x-4'>
+        <a href="#" className=''>Log In</a>
+        <a href="#"><button>Sign Up</button></a>
+        
+        </div>
       </div>
+      
     </>
   );
 }
