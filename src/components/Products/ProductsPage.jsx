86a33db55c8c6e1caee1ac93b5188ace7225984e
TechMrs <kemilat50@gmail.com>
2024-04-13 23:10:29 +0800

    Undone commit from branch Shaybeth, fixed all errors

--- src/components/Products/ProductsPage.jsx
+++ src/components/Products/ProductsPage.jsx
@@ -9,28 +9,10 @@ import Submenu from "./Submenu";
 const ITEMS_PER_PAGE = 12;
 
 const AllProducts = () => {
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filters, setFilters] = useState({
-    priceRange: null,
-    category: null,
-  });
-
-  const fetchProducts = async () => {
-    try {
-      // Make API call to fetch products
-      const response = await axios.get("https://kcoat.onrender.com/products");
-      console.log(response.data);
-      setSelectedProducts(response.data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      setIsLoading(false);
-    }
-  };
 
   // Fetch products on component mount
   useEffect(() => {
@@ -38,8 +20,8 @@ const AllProducts = () => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get("https://kcoat.onrender.com/products");
-        setProducts(response.data);
-        setFilteredProducts(response.data);
+        // setProducts(response.data);
+        setSelectedProducts(response.data);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching products:", error);
@@ -49,60 +31,14 @@ const AllProducts = () => {
     fetchProducts();
   }, []);
 
-  // Call filterProducts when searchQuery or filters change
-  useEffect(() => {
-    filterProducts();
-  }, [searchQuery, filters]);
-
-  const filterProducts = () => {
-    let filtered = products;
-
-    // Search query filter
-    if (searchQuery) {
-      filtered = filtered.filter((product) =>
-        product.ProductName.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-    }
-
-    // Price range filter
-    if (filters.priceRange) {
-      const [minPrice, maxPrice] = filters.priceRange;
-      filtered = filtered.filter(
-        (product) =>
-          product.ProductPrice >= minPrice && product.ProductPrice <= maxPrice
-      );
-    }
-
-    // Category filter
-    if (filters.category) {
-      filtered = filtered.filter(
-        (product) => product.Category.toLowerCase() === filters.category.toLowerCase()
-      );
-    }
-
-    setFilteredProducts(filtered);
-    setCurrentPage(1); // Reset current page when filter change
-  };
-
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
-  const handleFilterChange = (type, value) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [type]: value,
-    }));
-  };
-
   const indexOfLastProduct = currentPage * ITEMS_PER_PAGE;
   const indexOfFirstProduct = indexOfLastProduct - ITEMS_PER_PAGE;
-  const currentProducts = filteredProducts.slice(
+  const currentProducts = selectedProducts.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
 
-  const totalPages = Math.ceil(filteredProducts.length / ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(selectedProducts.length / ITEMS_PER_PAGE);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -116,19 +52,6 @@ const AllProducts = () => {
     }
   };
 
-
-  // Renders product cards
-//   const renderProductCards = currentProducts.map((item) => (
-//     <Link key={item.Productid} to={`/all-products/${item.Productid}`}>
-//       <Cards
-//         id={item.Productid}
-//         image={item.ProductImage}
-//         title={item.ProductName}
-//         price={item.ProductPrice}
-//       />
-//     </Link>
-  //));
-  
   const mostPopular = currentProducts.map((item) => {
     console.log("Product ID:", item.Productid); // Log the Productid
     return (
@@ -154,7 +77,7 @@ const AllProducts = () => {
               Categories
             </h2>
             <ul className="flex flex-col gap-[3em] font-oxygen w-full">
-              <Submenu setFilter={handleFilterChange} />
+              <Submenu setSelectedProducts={setSelectedProducts} />
             </ul>
           </div>
         </div>
@@ -173,12 +96,12 @@ const AllProducts = () => {
                 { label: "N25000 - N30000", range: [25000, 30000] },
                 { label: "N30000 - N35000", range: [30000, 35000] },
                 { label: "N35000 - N40000", range: [35000, 40000] },
-              ].map(({ label, range }, index) => (
+              ].map(({ label }, index) => (
                 <div key={index} className="flex gap-5 items-center">
                   <input
                     type="checkbox"
                     className="w-4 h-4 checkbox"
-                    onChange={() => handleFilterChange("priceRange", range)}
+                    // onChange={() => handleFilterChange("priceRange", range)}
                   />
                   <p>{label}</p>
                 </div>
@@ -194,18 +117,19 @@ const AllProducts = () => {
             Our Collection Of Products
           </h1>
           <div className="w-3/5">
-            <SearchInput onSearch={handleSearch} />
+            <SearchInput /> {/*onSearch={handleSearch} */}
           </div>
           <h3 className="font-oxygen font-bold text-secondary">
             Showing {indexOfFirstProduct + 1}-
-            {Math.min(indexOfLastProduct, filteredProducts.length)} of {filteredProducts.length} Items
+            {Math.min(indexOfLastProduct, selectedProducts.length)} of{" "}
+            {selectedProducts.length} Items
           </h3>
 
           {isLoading ? (
             <div className="loader"></div>
           ) : (
             <div className="grid grid-cols-3 justify-center gap-[4rem]">
-              {renderProductCards}
+              {mostPopular}
             </div>
           )}
         </div>
@@ -215,7 +139,8 @@ const AllProducts = () => {
           <div className="flex flex-col w-[50%] py-[5rem] gap-[1.6rem] items-center">
             <h3 className="font-oxygen font-bold text-secondary">
               Showing {indexOfFirstProduct + 1}-
-              {Math.min(indexOfLastProduct, filteredProducts.length)} of {filteredProducts.length} Items
+              {Math.min(indexOfLastProduct, selectedProducts.length)} of{" "}
+              {selectedProducts.length} Items
             </h3>
 
             <div className="w-full flex">
@@ -247,7 +172,11 @@ const AllProducts = () => {
                     onClick={handleNextPage}
                   >
                     View More
-                    <img src={angleRight} alt="View More" className="w-4 inline ml-1" />
+                    <img
+                      src={angleRight}
+                      alt="View More"
+                      className="w-4 inline ml-1"
+                    />
                   </button>
                 )}
               </div>
@@ -255,9 +184,8 @@ const AllProducts = () => {
           </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
-export default AllProducts;          
-
+export default AllProducts;
--- src/components/StaticComponents/Button.jsx
+++ src/components/StaticComponents/Button.jsx
@@ -14,3 +14,72 @@ const Button = ({children, className}) => {
 }
 
 export default Button;
+
+
+  // const [products, setProducts] = useState([]);
+  // const [filteredProducts, setFilteredProducts] = useState([]);
+
+ // const [searchQuery, setSearchQuery] = useState("");
+  // const [filters, setFilters] = useState({
+  //   priceRange: null,
+  //   category: null,
+  // });
+
+  // const fetchProducts = async () => {
+  //   try {
+  //     // Make API call to fetch products
+  //     const response = await axios.get("https://kcoat.onrender.com/products");
+  //     console.log(response.data);
+  //     setSelectedProducts(response.data);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     console.error("Error fetching products:", error);
+  //     setIsLoading(false);
+  //   }
+  // };
+
+//  useEffect(() => {
+//    filterProducts();
+//  }, [searchQuery, filters]);
+
+//  const filterProducts = () => {
+//    let filtered = products;
+
+//    // Search query filter
+//    if (searchQuery) {
+//      filtered = filtered.filter((product) =>
+//        product.ProductName.toLowerCase().includes(searchQuery.toLowerCase())
+//      );
+//    }
+
+//    // Price range filter
+//    if (filters.priceRange) {
+//      const [minPrice, maxPrice] = filters.priceRange;
+//      filtered = filtered.filter(
+//        (product) =>
+//          product.ProductPrice >= minPrice && product.ProductPrice <= maxPrice
+//      );
+//    }
+
+//    // Category filter
+//    if (filters.category) {
+//      filtered = filtered.filter(
+//        (product) =>
+//          product.Category.toLowerCase() === filters.category.toLowerCase()
+//      );
+//    }
+
+//    setFilteredProducts(filtered);
+//    setCurrentPage(1); // Reset current page when filter change
+//  };
+
+//  const handleSearch = (query) => {
+//    setSearchQuery(query);
+//  };
+
+//  const handleFilterChange = (type, value) => {
+//    setFilters((prevFilters) => ({
+//      ...prevFilters,
+//      [type]: value,
+//    }));
+//  };
